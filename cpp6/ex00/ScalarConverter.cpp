e6c499307e08de8d8a58db9c94129d08c8b98e5f
aamraouy <186086629+amroabd11@users.noreply.github.com>
2026-06-21 23:09:54 +0100

    more tests for ex00

--- cpp6/ex00/ScalarConverter.cpp
+++ cpp6/ex00/ScalarConverter.cpp
@@ -1,38 +1,44 @@
 #include "ScalarConverter.hpp"
 #include <cstring>
 #include <cstdlib>
+#include <iomanip>
 #include <climits>
 #include <cfloat>
 
-//void	convert_toChar(std::string& ch)
-//{
-//	char one = str[1];
-//		if (std::isprint(one))
-//		{
-//			std::cout << "char: " << str[1]<<std::endl;
-//			std::cout << "int: " << static_cast<int>(str[1])<<std::endl;
-//			std::cout << "float: "<< static_cast<float>(str[1])<<"f" << std::endl;
-//			std::cout << "double: "<< static_cast<double>(str[1]) << std::endl;
-//		}
-//		else{
-//			std::cout << "char : Non displayable"<<std::endl;
-//			std::cout << "int: " << static_cast<int>(str[1])<<std::endl;
-//			std::cout << "float: " << static_cast<float>(str[1])<< "f"<<std::endl;
-//			std::cout << "double: " << static_cast<double>(str[1])<<std::endl;
-//		}
-//}
+void	specialLiteral(std::string& str)
+{
+	std::cout <<"char: impossible"<<std::endl;
+	std::cout << "int: impossible"<<std::endl;
+	if (str == "nanf" || str == "+inff" || str == "-inff")
+	{
+		std::cout << "float: "<<str<<std::endl;
+		if (str == "nanf")
+			std::cout << "double: nan"<<std::endl;
+		else if (str == "+inff")
+			std::cout << "double: +inf"<<std::endl;
+		else
+			std::cout << "double: -inf"<<std::endl;
+	}
+	else{
+		std::cout << "float: "<< str<<"f"<<std::endl;
+		std::cout << "double: "<< str<<std::endl;
+	}
+}
 
 void	ScalarConverter::convert( std::string &str)
 {
-//	if ()//TODO THIS IF will handle the special cases such as inf -inf nanf -nanf
-//	{}
+	std::cout <<std::fixed << std::setprecision(1);
+	if (str == "nan" || str == "nanf" || str == "+inf" || str == "-inf" || str == "+inff" || str == "-inff"){
+		specialLiteral(str);
+		return ;
+	}
 	char *end;
 	if (str.length() == 3 && str[0] == '\'' && str[2] == '\'')
 	{
 		std::cout << "char: "<<str[1]<<std::endl;
 		std::cout << "int: "<<static_cast<int>(str[1])<<std::endl;
 		std::cout << "float: "<<static_cast<float>(str[1])<<"f"<<std::endl;
-		std::cout << "char: "<<static_cast<double>(str[1])<<std::endl;
+		std::cout << "double: "<<static_cast<double>(str[1])<<std::endl;
 		return ; 
 	}
 	double ret = std::strtod(str.c_str(), &end);
@@ -41,25 +47,32 @@ void	ScalarConverter::convert( std::string &str)
 		std::cout << "'"<<end<<"'"<<std::endl;
 		throw std::runtime_error("wrong intput stupid hacker!!");
 	}
-	if (ret >= 32 && ret <= 126 ){
-		std::cout << "char: "<<static_cast<char>(ret)<<std::endl;
-		std::cout << "int: "<<static_cast<int>(ret)<<std::endl;
-		std::cout << "float: "<<static_cast<float>(ret)<<"f"<<std::endl;
-		std::cout << "double: "<<static_cast<double>(ret)<<std::endl;
-	}
-	else if(ret > DBL_MAX || ret < DBL_MIN){
-		std::cout << DBL_MAX<<std::endl;
-		std::cout << DBL_MIN<<std::endl;
-		std::cout << "no conversion possible the num is greather then doubles"<<std::endl;
+	if (ret >= 0 && ret <= 126 ){
+		if (std::isprint(static_cast<int>(ret))){
+			std::cout << "char: "<<static_cast<char>(ret)<<std::endl;
+			std::cout << "int: "<<static_cast<int>(ret)<<std::endl;
+			std::cout << "float: "<<static_cast<float>(ret)<<"f"<<std::endl;
+			std::cout << "double: "<<static_cast<double>(ret)<<std::endl;
+		}
+		else{
+			std::cout << "char: Non displayable"<<std::endl;
+			std::cout << "int: "<<static_cast<int>(ret)<<std::endl;
+			std::cout << "float: "<<static_cast<float>(ret)<<"f"<<std::endl;
+			std::cout << "double: "<<static_cast<double>(ret)<<std::endl;
+		}
 	}
 	else if(ret > INT_MAX || ret < INT_MIN ){
 		std::cout << "char: impossible"<<std::endl;
-		std::cout << "int: out of int bounds"<<std::endl;
+		std::cout << "int: impossible"<<std::endl;
 		std::cout << "float: "<<static_cast<float>(ret)<<"f"<<std::endl;
 		std::cout << "double: "<<static_cast<double>(ret)<<std::endl;
 	}
-	else if(ret > DBL_MAX || ret < DBL_MIN)
-		std::cout << "all conversions are impossible"<<std::endl;
+	else if (ret < INT_MAX && ret > INT_MIN){
+		std::cout << "char: impossible"<<std::endl;
+		std::cout << "int: "<<static_cast<int>(ret)<<std::endl;
+		std::cout<<  "float: " <<static_cast<float>(ret)<<"f"<<std::endl;
+		std::cout<< "double: " <<static_cast<double>(ret)<<std::endl;
+	}
 
 }
 
