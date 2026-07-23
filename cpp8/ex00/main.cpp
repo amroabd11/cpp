348028f507e8279fdf36fd5d3e0d5ce3c9d2b34b
Abdelghani Amraouy <186086629+amroabd11@users.noreply.github.com>
2026-07-23 15:16:35 +0100

    ex00 small add

--- cpp8/ex00/main.cpp
+++ cpp8/ex00/main.cpp
@@ -1,4 +1,5 @@
 #include "easyfind.hpp"
+#include <list>
 
 int main()
 {
@@ -9,12 +10,24 @@ int main()
 	vec.push_back(341);
 	vec.push_back(3);
 	vec.push_back(0);
+	std::list<int> lis;
+	lis.push_back(1);
+	lis.push_back(10);
+	lis.push_back(100);
+	lis.push_front(-11);
 
-
+//	std::vector<int>::iterator it1;
+//	it1 = vec.begin();
+//	it1 += 3;
+//	std::list<int>::iterator it;
+//	it = lis.begin();
+//	it--;
+//	std::cout << "iterator: "<< *it1<<std::endl;
 	const std::vector<int> const_vec = const_cast<std::vector<int>&>(vec);
 	try{
 		easyfind(vec, 10);
 		easyfind(const_vec, 1);
+		easyfind(lis, -110);
 	}
 	catch(std::exception& e)
 	{
