ba63322c806bbd4314cbfab4541f6920fc51c3de
aamraouy <186086629+amroabd11@users.noreply.github.com>
2026-07-30 12:57:33 +0100

    still parsing , noting in logic added

--- cpp9/ex00/BitcoinExchange.cpp
+++ cpp9/ex00/BitcoinExchange.cpp
@@ -1,6 +1,18 @@
 #include "BitcoiExchange.hpp"
 
-void parse_data(std::ifstream& file)
+void parse_data(char* file, std::ifstream& inputfile)
 {
-	
+	if (!file)
+		throw std::runtime_error("Error: could not open file.");
+	inputfile.open(file);
+	if (!inputfile.is_open())
+		throw std::runtime_error("Error: could not open file.");
+	std::string line;
+	std::getline(inputfile, line);
+	if (line != "date | value")
+		throw std::runtime_error("first line must be 'date | value'");
+	while(getline(inputfile,line))
+	{
+
+	}
 }
--- cpp9/ex00/BitcoinExchange.hpp
+++ cpp9/ex00/BitcoinExchange.hpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <algorithm>
 
+typedef std::map<std::string, long> mymap;
 
-void parse_data(std::ifstream& file);
+void parse_data(char * file, std::ifstream& inputfile);
 
