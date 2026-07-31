c47ddc34d96bff10eaf2f5f42dbadf122503f69e
aamraouy <186086629+amroabd11@users.noreply.github.com>
2026-07-31 13:14:36 +0100

    working on ex00

--- cpp9/ex00/BitcoinExchange.cpp
+++ cpp9/ex00/BitcoinExchange.cpp
@@ -1,25 +1,88 @@
 #include "BitcoinExchange.hpp"
 
-void parse_data(std::ifstream& inputfile, mymap& inputmap, char delim)
+
+
+void parse_csv(mymap& dbmap)
+{
+	std::ifstream db("data.csv");
+	if (!db.is_open())
+		throw std::runtime_error("Error: could not open csv file");
+	std::string line;
+	std::getline(db, line);
+	if (line != "date,exchange_rate")
+		throw std::runtime_error("Error: wrong header");
+	while(getline(db, line))
+	{
+		size_t pos = line.find(',');
+		if (pos == std::string:npos)
+			continue;
+		std::string date = line.substr(0,pos);
+		std::string value = line.substr(pos+1);
+		std::err;
+		if (!ValidDate(date))
+			continue;
+		char *end;
+		double num = std::strtod(value.c_str(), &end);
+		if (*end != '\0' || value.empty())
+			continue;
+		dbmap[date] = num;
+	}
+
+}
+
+double ValidValue(std::string value, double& val)
+{
+	char* end;
+	val = std::strtod(value.c_str(), &end);
+	if( *end!='\0' || value.empty())
+}
+
+void process_data(std::ifstream& inputfile, mymap& dbmap)
 {
 	if (!inputfile.is_open())
 		throw std::runtime_error("Error: could not open file.");
 	std::string line;
 	std::getline(inputfile, line);
-	if (line != "date | value" || line != "date,exchange_rate")
-		throw std::runtime_error("header is wrong");
-	//std::stringstream token;
+	if (line != "date | value")
+		throw std::runtime_error("Error: header is wrong");
+
 	while(getline(inputfile,line))
 	{
-		std::stringstream token(line); 
-		std::string date;
+		stringstream tokens(line);
+
 		std::string delim;
+		std::string date;
 		std::string value;
-		token >> date >> delim >> value;
+		token >>date>>delim>>value;
 
-		std::cout << date <<", "<<delim<<", " <<value<<std::endl;
-		// TODO i should not quite if one of them isn't present but i should print error;
+		double val;
+		if (delim != "|" && !ValidDate(date)){
+			std::cout << "Error: bad input => " <<line<<std::endl;
+			continue;
+		}
+		if (!ValidValue(value, val))
+		{
+			if (val < 0)
+				std::cout << "Error: not a positive number"<<std::endl;
+			else if (val >1000)
+				std::cout << "Error: too large a number"<<std::endl;
+			else
+				std::cout << "Error: bad input => "<<line <<std::endl;
+			continue;
+		}
 
-		inputmap[date] = value;
+
+		
+		mymap::iterator it = dbmap.lower_bound(date);
+		if (it == dbmap.end() || it->first != date)
+		{
+			if (it == dbmap.begin())
+			{
+				std::cout << "Error: no earlier date in database => " << line<<std::endl;
+				continue;
+			}
+			--it;
+		}
+		std::cout << date << " => "<< value<<" = "<< (val * it->second)<<std::endl;
 	}
 }
--- cpp9/ex00/BitcoinExchange.hpp
+++ cpp9/ex00/BitcoinExchange.hpp
@@ -5,7 +5,7 @@
 #include <fstream>
 #include <sstream>
 
-typedef std::map<std::string, std::string> mymap;
+typedef std::map<std::string, double> mymap;
 
 void parse_data(std::ifstream&, mymap&, char);
 
